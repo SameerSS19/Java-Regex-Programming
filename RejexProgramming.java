31c700fd6dbc3dc6742c364b1a75aa05edba71b9
SameerSS19 <sk3090019@gmail.com>
2021-05-22 12:43:01 +0530

    Make sure 400088 ia also valid

--- RejexProgramming.java
+++ RejexProgramming.java
@@ -1,22 +1,28 @@
 package com.rejexprogram;
-import java.util.Scanner;
+import java.util.regex.*;
 public class RejexProgramming 
 {
-	public static void pincode()
-	{
-		System.out.println("Welcome to Java Patterns or Regular Expressions");
-		System.out.println("Enter the Pincode");
-		Scanner scan = new Scanner(System.in);
-		String pincode = scan.nextLine();
-		scan.nextLine();
+	public static boolean validatePin(String pinCode) {
 		String regex = "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$";
-		System.out.println("The zip code is: " + pincode);
-	    System.out.println("Check Indian post the Pin code is correct ? " + pincode.matches(regex)); 
-	}
-	public static void main(String[] args) 
-	{
-	pincode();
-	}
-
+		
+		Pattern p = Pattern.compile(regex);
+		
+		if (pinCode == null) {
+			System.out.println("Invalid PIN");	
+		}
+		Matcher m = p.matcher(pinCode);
+		return m.matches();
+		 }
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to the PIN validation program");
+		String pin1 = "400088";
+		 System.out.println(pin1+" "+validatePin(pin1));
+		 String pin2 = "A400088";
+		 System.out.println(pin2+" "+validatePin(pin2));
+		 String pin3 = "400088B";
+		 System.out.println(pin3+" "+validatePin(pin3));
+		 String pin4 = "400 088";
+		 System.out.println(pin4+" "+validatePin(pin4));
 }
-
+}
