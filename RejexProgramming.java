885368ebedfeaccb53117dab505275dc8d1511f9
SameerSS19 <sk3090019@gmail.com>
2021-05-22 11:12:16 +0530

    Welcome to Java Regex Programming

---
+++ RejexProgramming.java
@@ -0,0 +1,9 @@
+package com.rejexprogram;
+
+public class RejexProgramming {
+	public static void main(String[] args) {
+	System.out.println("Welcome to Java Patterns or Regular Expressions");
+	}
+
+}
+
